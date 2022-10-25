d7cd2a3addc208f54a10967eb08491bfea8bb56f
Dgsuyob <davidsuyob@gmail.com>
2022-10-24 22:20:28 -0400

    añadiendo clases

---
+++ Source/arkanoid/Paddle.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Paddle.generated.h"
+
+class UFloatingPawnMovement;
+UCLASS()
+class ARKANOID_API APaddle : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APaddle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UStaticMeshComponent* SM_Padle;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UFloatingPawnMovement* FloatingMovement;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime);
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent);
+
+	virtual void MoveHorizontal(float AxisValue);
+};
