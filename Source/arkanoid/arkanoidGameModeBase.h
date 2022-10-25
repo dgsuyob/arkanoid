b7e781c825e344f27679442956b28a979ce55462
Dgsuyob <davidsuyob@gmail.com>
2022-10-24 22:38:25 -0400

    creando ladrillos en arkanoidGameModeBase

--- Source/arkanoid/arkanoidGameModeBase.h
+++ Source/arkanoid/arkanoidGameModeBase.h
@@ -1,17 +1,24 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
-#include "arkanoidGameModeBase.generated.h"
+#include "ArcanoidGameModeBase.generated.h"
 
 /**
- * 
+ *
  */
+
+class ABrick;
+
 UCLASS()
-class ARKANOID_API AarkanoidGameModeBase : public AGameModeBase
+class ARCANOID_API AArcanoidGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void BeginPlay() override;
+
+	ABrick* ladrillo01;
+
 };
